5b82ad59ef85dfb64dcd4635c9542974578984a7
IntellyJay <intellyjay@gmail.com>
2025-07-24 11:51:36 +0900

    Created: pages/*.vue

---
+++ pages/main.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <template v-if="isMobile">
+            <div>모바일 페이지</div>
+        </template>
+        <template v-else>
+            <Loading v-if="loading" />
+            <Main v-else :dataset="dataSet" />
+        </template>
+    </div>
+</template>
+
+<script>
+// import Loading from '../components/Loading.vue';
+
+export default {
+    data() {
+        return {
+            loading: true,
+            dataSet: {
+                data: '메인페이지!',
+            },
+        }
+    },
+    created() {
+        // 글로벌 변수값 변경
+        this.globalValue = '페이지밸류';
+    },
+    mounted() {
+        setTimeout(() => {
+            this.loading = false;
+        }, 1000);
+    },
+    methods: {
+
+    }
+}
+</script>
